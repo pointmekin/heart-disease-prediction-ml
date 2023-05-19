60cd27ec8ca3dc1a1805cf5b5c7237c2677d8a7e
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-05-20 03:32:48 +0700

    feat: add model

--- api/index.py
+++ api/index.py
@@ -1,14 +1,33 @@
+import os
 import joblib
 import pandas as pd
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-model = joblib.load('./model.joblib')
+model_path = os.path.join(os.getcwd(), 'api', 'model.joblib')
+model = joblib.load(model_path)
 
+# Define a simple route
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello():
+    return 'Heart disease prediction API using decision tree model'
 
-@app.route('/about')
-def about():
-    return 'About'
+# Define a route that accepts POST requests and returns JSON data
+@app.route('/api/prediction', methods=['POST'])
+def submit_data():
+    # Assuming the request body is in JSON format
+    data = request.get_json()
+    # Process the data...
+
+    # convert to dataframe
+    df = pd.DataFrame(data, index=[0])
+
+    print(type(df["general_health"][0]))
+
+    # Return a response
+    prediction = {
+        'prediction': int(model.predict(df.iloc[0:1])[0]),
+        'confidence': model.predict_proba(df.iloc[0:1])[0].tolist()  # Convert ndarray to list
+    }
+    print (prediction)
+    return jsonify(prediction)
